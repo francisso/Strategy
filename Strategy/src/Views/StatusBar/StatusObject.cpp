89064073e5c827177f90021717d03bd099603cdc
emvanekk95/em <emvanekmatveev@yandex.ru/em>
2015-05-06 02:35:41 +0300

    Забыл пару файлов

---
+++ Strategy/src/Views/StatusBar/StatusObject.cpp
@@ -0,0 +1,71 @@
+/*
+ * StatusObject.cpp
+ *
+ *  Created on: 05 мая 2015 г.
+ *      Author: ivanmatveev
+ */
+
+#include "StatusObject.h"
+#include "../../Drawable/Draw.h"
+
+StatusObject::StatusObject(Drawable* Image, SDL_Rect Rect_text) {
+	image = Image;
+	SDL_Rect src = {0, 0, 200, 200};
+	text_image = new Draw(src, "res/images/for_text.bmp");
+	rect_text = Rect_text;
+	text_image->SetDestRect(&Rect_text);
+	color.b = 255;
+	color.g = 255;
+	color.r = 255;
+	color.unused = 255;
+	size_text = 16;
+}
+
+StatusObject::~StatusObject() {
+	delete image;
+	CreateTextImage();
+}
+void StatusObject::SetText(std::string str) {
+	text = str;
+	CreateTextImage();
+}
+void StatusObject::SetText(const char* str) {
+	text = str;
+	CreateTextImage();
+}
+Drawable* StatusObject::GetTextImage() {
+	return text_image;
+}
+
+Drawable* StatusObject::GetImage() {
+	return image;
+}
+
+std::string StatusObject::GetText() {
+	return text;
+}
+
+SDL_Rect* StatusObject::GetTextRect() {
+	return &rect_text;
+}
+void StatusObject::CreateTextImage(){
+	// Загружаем шрифт по заданному адресу размером sz
+	TTF_Font * fnt = TTF_OpenFont("res/font/courier.ttf", size_text);
+	// Переносим на поверхность текст с заданным шрифтом и цветом
+	text_image->SetImage( TTF_RenderText_Blended(fnt, text.c_str(), color));
+	TTF_CloseFont(fnt); // Закрываем шрифт
+}
+void StatusObject::SetSizeText(int size){
+	size_text = size;
+}
+
+void StatusObject::SetColorText(SDL_Color clr){
+	color = clr;
+}
+void StatusObject::SetColorText(int red, int green, int blue){
+	color.r = static_cast<Uint8>(red);
+	color.g = static_cast<Uint8>(green);
+	color.b = static_cast<Uint8>(blue);
+}
+
+
---
+++ Strategy/src/Views/StatusBar/StatusObject.h
@@ -0,0 +1,62 @@
+/*
+ * StatusObject.h
+ *
+ *  Created on: 05 мая 2015 г.
+ *      Author: ivanmatveev
+ */
+
+#ifndef VIEWS_STATUSBAR_STATUSOBJECT_H_
+#define VIEWS_STATUSBAR_STATUSOBJECT_H_
+
+#include "IStatusObject.h"
+#include <string>
+
+class StatusObject: public IStatusObject {
+public:
+	StatusObject(Drawable* Image, SDL_Rect Rect_text);
+	virtual ~StatusObject();
+	/**
+	 * Кэшируя, возвращает Drawable* с текстом
+	 * @return картинку с текстом
+	 */
+	virtual Drawable* GetTextImage();
+
+	/**
+	 * Картинка, отобращающаяся рядом с текстом.
+	 * Должна кэшироваться.
+	 * В себе хранит координаты относительно строки состояния
+	 */
+	virtual Drawable* GetImage();
+	/**
+	 * @return текст, который будет отображаться
+	 */
+	virtual std::string GetText();
+	/*
+	 * Установка текста
+	 */
+	void SetText(std::string str);
+	void SetText(const char* str);
+
+	// Установка размера шрифта
+	void SetSizeText(int size);
+	// Установка цвета
+	void SetColorText(SDL_Color clr);
+	void SetColorText(int red, int green, int blue);
+private:
+	/**
+	 * Координаты прямоугольника относительно начала строки состояния
+	 * @return прямоугольник, в который рисуется текст.
+	 */
+	virtual SDL_Rect* GetTextRect();
+	void CreateTextImage();
+	std::string text;
+	Drawable* image;
+	Drawable* text_image;
+	SDL_Rect rect_text;
+	// цвет текста
+	SDL_Color color;
+	// размер текста
+	int size_text;
+};
+
+#endif /* VIEWS_STATUSBAR_STATUSOBJECT_H_ */
